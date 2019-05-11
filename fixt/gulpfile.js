0db48c321ec66fdd55d29bf48de66bdc143c7cb8
Strider <strange.transistor@gmail.com>
2019-05-12 00:00:54 +0300

    prefab: frontend.

--- fixt/gulpfile.js
+++ fixt/gulpfile.js
@@ -2,22 +2,11 @@
 require('console-ultimate')
 
 
-var { parallel } = require('gulp')
-
-
-var Css = require('../rule/css')
-var Html = require('../rule/html')
-var JavaScript = require('../rule/javascript')
-var Digest = require('../rule/digest')
+var Frontend = require('../prefab/frontend')
 
 var Context = require('../util/Context')
 var context = Context()
 context.describe()
 
 
-var css = Css(context)
-var html = Html(context)
-var javascript = JavaScript(context)
-var digest = Digest(context)
-
-exports.default = parallel(css, html, javascript, digest)
+exports.default = Frontend(context)
---
+++ prefab/frontend.js
@@ -0,0 +1,18 @@
+
+var { parallel } = require('gulp')
+
+var Css = require('../rule/css')
+var Html = require('../rule/html')
+var JavaScript = require('../rule/javascript')
+var Digest = require('../rule/digest')
+
+
+module.exports = function frontend (context)
+{
+	var css = Css(context)
+	var html = Html(context)
+	var javascript = JavaScript(context)
+	var digest = Digest(context)
+
+	return parallel(css, html, javascript, digest)
+}
