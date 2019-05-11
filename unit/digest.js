d4d38b6e46852effe845c365ee44bd14b1a0eaaf
Strider <strange.transistor@gmail.com>
2019-05-11 23:52:13 +0300

    full digest.

--- unit/digest.js
+++ unit/digest.js
@@ -8,8 +8,22 @@ var rev = require('../util/rev')
 
 module.exports = function digest ()
 {
-	var
-	_ = rev()
+	var _ = {}
+
+	_.version = '0' // context.version
+
+	_.timestamp = (new Date).toISOString()
+
+	_.rev = rev()
+
+	// context.instance
+	// context.hash
+
+	/*
+	release.instance = instance
+	release.name = env.package.name + '-' + instance
+	*/
+
 	_ = dump(_)
 
 	return file('release.json', _, { src: true })
