c9e8097d78d7ef8618e5a2200550504c64879a7e
Strider <strange.transistor@gmail.com>
2019-05-20 02:16:46 +0300

    hash.

--- prefab/frontend.js
+++ prefab/frontend.js
@@ -8,10 +8,14 @@ var Html = require('../rule/html')
 var JavaScript = require('../rule/javascript')
 var Digest = require('../rule/digest')
 
+var get_hash = require('../util/get-hash')
+
 
 module.exports = function frontend (context)
 {
 	context.describe()
+
+	context.opts.hash = get_hash(context.opts)
 
 	var clean = Clean(context)
 	var css = Css(context)
---
+++ util/get-hash.js
@@ -0,0 +1,20 @@
+
+var randomstring = require('randomstring').generate
+
+var fallback = require('./get-fallback')
+
+
+module.exports = function get_hash (options)
+{
+	return fallback(options, 'hash', () => random())
+}
+
+
+function random ()
+{
+	return randomstring(
+	{
+		length: 7,
+		capitalization: 'lowercase',
+	})
+}
