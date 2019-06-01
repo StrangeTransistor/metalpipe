d0df4aa9f7043602abbc6a8cee1f07fdaad9c175
Strider <strange.transistor@gmail.com>
2019-06-01 21:11:20 +0300

    copy origin to tmp.

--- test/origin.js
+++ test/origin.js
@@ -2,12 +2,17 @@
 var tmp_rootpath  = require('./tmp-rootpath')
 var fixt_rootpath = require('./fixt-rootpath')
 
+var { copySync: cp } = require('fs-extra')
+
 
 module.exports = (name) =>
 {
 	var tmp  = tmp_rootpath()
 	var fixt = fixt_rootpath(name)
 
-	console.warn(tmp())
-	console.warn(fixt())
+	cp(fixt(), tmp())
+
+	console.info('Origin: from `%s` to `%s`', fixt(), tmp())
+
+	return tmp()
 }
