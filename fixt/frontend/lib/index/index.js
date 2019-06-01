ab0ad0c733e602072cd3247a0609577e4753cc84
Strider <strange.transistor@gmail.com>
2019-06-02 01:50:56 +0300

    frontend: start test things one by one, test cjs and es imports.

--- fixt/frontend/lib/index/index.js
+++ fixt/frontend/lib/index/index.js
@@ -1,28 +1,33 @@
 
-import not from 'ramda/es/not'
+import answer from 'the-answer'
+console.log('answer', answer)
 
-console.log('not', not(false))
+import noop from 'noop3'
+console.log('noop', noop)
 
-import '~lib/other/other'
+import { curry } from 'rambda/src/curry'
+console.log('curry', curry)
 
-console.log(!! global.global)
-
-import { join } from 'path'
-
-console.log(join('a', 'b'))
-
-import mst from '../other/other.mst.html'
-
-console.log(mst.render({ data: 'yes' }))
-
-import pug_s from '../other/some.static.pug'
-
-console.log(pug_s)
-
-import pug from './index.pug'
-
-console.log(pug({ other: 'Other', some: 'Some' }))
-
-var x: string = 'yes'
-
-console.log(x)
+//import '~lib/other/other'
+//
+//console.log(!! global.global)
+//
+//import { join } from 'path'
+//
+//console.log(join('a', 'b'))
+//
+//import mst from '../other/other.mst.html'
+//
+//console.log(mst.render({ data: 'yes' }))
+//
+//import pug_s from '../other/some.static.pug'
+//
+//console.log(pug_s)
+//
+//import pug from './index.pug'
+//
+//console.log(pug({ other: 'Other', some: 'Some' }))
+//
+//var x: string = 'yes'
+//
+//console.log(x)
