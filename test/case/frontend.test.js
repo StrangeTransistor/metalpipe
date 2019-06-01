992d48c63eecb5f78f9a081d9c58beb0b9ea5cc9
Strider <strange.transistor@gmail.com>
2019-06-01 22:29:00 +0300

    note for digest.

--- test/case/frontend.test.js
+++ test/case/frontend.test.js
@@ -5,6 +5,7 @@ var collate = require('../collate-rootpath')
 
 var run = require('../run')
 var compare = require('../compare-release')
+// var digest = require('../expect-digest')
 
 
 describe('Frontend', () =>
@@ -17,6 +18,7 @@ describe('Frontend', () =>
 		run('gulp --once', tmp)
 
 		expect(compare(cl(), tmp('release/dev'))).ok
+		// digest(tmp('release/dev/release.json'), { hash: true })
 	})
 
 	xit('final', () =>
---
+++ test/expect-digest.js
@@ -0,0 +1,34 @@
+
+var { expect } = require('chai')
+
+var { readJsonSync: read } = require('fs-extra')
+
+
+module.exports = (path, options) =>
+{
+	options = { ...options }
+
+	var release = read(path)
+
+	expect(release).an('object')
+	expect(release.version).eq('0.0.0')
+	expect(release.timestamp).a('string')
+
+	if (options.instance)
+	{
+		expect(release.instance).a('string')
+		expect(release.name).a('string')
+	}
+
+	if (options.hash)
+	{
+		expect(release.hash).a('string')
+		expect(release.hash).eq('fixed')
+	}
+
+	expect(release.git).an('object')
+	expect(release.git.rev).a('string')
+	expect(release.git.msg).a('string')
+	expect(release.git.timestamp).a('string')
+	expect(release.git.author).a('string')
+}
