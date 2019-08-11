5c642bc27af8ad7beac959f60798a0fb97a7b298
Strider <strange.transistor@gmail.com>
2019-08-11 19:38:25 +0300

    prefab: library.

---
+++ prefab/library.js
@@ -0,0 +1,44 @@
+
+var Clean = require('../pipeline/clean')
+var WithPackage = require('../pipeline/backend/with-package')
+var Javascript = require('../pipeline/javascript/single')
+var Other = require('../pipeline/backend/other')
+// var Digest = require('../pipeline/digest')
+
+
+module.exports = function frontend (context)
+{
+	context.describe()
+
+	var { series, parallel } = context.gulp
+
+	var clean = Clean(context)
+	var pkg = WithPackage(context)
+	var javascript = Javascript(context, { ignore })
+	var other = Other(context, { ignore })
+	// var digest = Digest(context)
+
+	return series(
+		clean,
+		parallel(
+			pkg,
+			javascript,
+			other
+			// digest
+		)
+	)
+}
+
+var ignore =
+[
+	'!package.json',
+	'!node_modules/**',
+	'!release/**',
+	'!gulpfile.js',
+
+	// '!web/**',
+
+	'!*npm-debug.log',
+	'!coverage/**',
+	'!flow-typed/**',
+]
