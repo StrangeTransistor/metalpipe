e066aec25db475b55c90504f0d86d36b40db6cf5
Strider <strange.transistor@gmail.com>
2019-08-17 00:59:55 +0300

    optimize test perf (origin symlink node_modules).

--- test/origin.js
+++ test/origin.js
@@ -15,11 +15,19 @@ module.exports = (name) =>
 	var tmp  = tmp_rootpath()
 	var fixt = fixt_rootpath(name)
 
-	// TODO: filter
-	cp(fixt(), tmp())
-
+	/* generic */
 	ln(root('node_modules'), tmp('../node_modules'))
+
+	/* this */
+	cp(fixt(), tmp(),
+	{
+		filter (path)
+		{
+			return (! path.match('node_modules'))
+		}
+	})
 	ln(root('.git'), tmp('.git'))
+	ln(fixt('node_modules'), tmp('node_modules'))
 
 	console.info('Origin: from `%s` to `%s`', fixt(), tmp())
 
