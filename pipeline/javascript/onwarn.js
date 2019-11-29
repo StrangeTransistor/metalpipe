3f0a14920af890b6fa169fed2b29901d70a377d1
Strider <strange.transistor@gmail.com>
2019-11-29 03:02:06 +0300

    tune onwarn.

--- pipeline/javascript/onwarn.js
+++ pipeline/javascript/onwarn.js
@@ -2,5 +2,7 @@
 
 module.exports = function onwarn (warning)
 {
-	console.debug('Rollup: %s (%s)', warning.toString(), warning.code)
+	// https://github.com/rollup/rollup/blob/master/src/rollup/types.d.ts
+	// console.log(warning.chunk)
+	console.debug('Rollup (%s): %s', warning.code, warning.toString())
 }
