9367f2019371632cb6d6914b6f71b32cb18d8b26
Strider <strange.transistor@gmail.com>
2019-12-26 20:10:16 +0300

    prep for outlander + node.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 
 module.exports =
 {
+	// extends: require.resolve('js-outlander/node'), // TODO: turn on
 	extends: require.resolve('js-outlander'),
 }
--- util/json.js
+++ util/json.js
@@ -1,12 +1,12 @@
 
 
-exports.load = function load (_)
+module.exports.load = function load (_)
 {
 	return JSON.parse(_)
 }
 
 
-exports.dump = function dump (_)
+module.exports.dump = function dump (_)
 {
 	return JSON.stringify(_, null, '  ')
 }
