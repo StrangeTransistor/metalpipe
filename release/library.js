9502e7b1bb08daf7eb57a6144c2f3f7638d282e5
Strider <strange.transistor@gmail.com>
2020-01-04 02:34:57 +0300

    first approach to d.ts generation.

--- release/library.js
+++ release/library.js
@@ -4,6 +4,7 @@ var WithPackage = require('../pipeline/library/with-package')
 var Javascript = require('../pipeline/javascript/single')
 var Other = require('../pipeline/other')
 
+var is_typescript = require('../pipeline/javascript/is-typescript')
 
 var Ignore = require('../util/Ignore')
 
@@ -11,6 +12,8 @@ var Ignore = require('../util/Ignore')
 module.exports = function frontend (context)
 {
 	context.describe()
+
+	context.typescript = is_typescript(context)
 
 	var ignore = Ignore()
 	ignore.test_aware(context)
@@ -24,10 +27,43 @@ module.exports = function frontend (context)
 
 	return series(
 		clean,
+		Ts(context, { ignore }),
 		parallel(
 			pkg,
 			javascript,
 			other
 		)
 	)
+}
+
+
+var { src } = require('gulp')
+var { dest: dst } = require('gulp')
+var nothing = require('../unit/nothing')
+
+
+function Ts (context, { ignore })
+{
+	console.warn(context.typescript)
+	if (! context.typescript)
+	{
+		return () => nothing().end()
+	}
+
+	var Typescript = require('gulp-typescript')
+
+	return function DTS ()
+	{
+		var { $from, $to } = context
+
+		var ignored = ignore.view()
+		var
+		glob = [ '**/*.ts', '!**/*.d.ts', ...ignored ]
+		glob = glob.map(glob => $from(glob))
+
+		var streams = src(glob)
+		.pipe(Typescript({ declaration: true }))
+
+		return streams.dts.pipe(dst($to()))
+	}
 }
