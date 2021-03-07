910b6d495732fc8b3d738bab60a67f0250e1836b
Strider <strange.transistor@gmail.com>
2021-03-07 21:15:09 +0300

    test cases.

---
+++ test/collate/backend/dev/src/m2.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var React = require('react');
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
+
+function Foo ()
+{
+	return React__default['default'].createElement('div', { class: "foo",} )
+}
+
+module.exports = Foo;
