e1806c48a3b38185fc4e97031f3b453b332e9a8d
Strider <strange.transistor@gmail.com>
2021-03-10 01:49:35 +0300

    test sucrase enum.

--- fixt/library-typescript/src/mod.ts
+++ fixt/library-typescript/src/mod.ts
@@ -6,3 +6,11 @@ export default function mod (s: Specific)
 {
 	console.log('mod', s)
 }
+
+enum E1 { A, B, C }
+function f1 (e: E1) { return e }
+f1(E1.A)
+
+const enum E2 { A, B, C }
+function f2 (e: E2) { return e }
+f2(E2.A)
