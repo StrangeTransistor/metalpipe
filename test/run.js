49c92f7998f8c8f5008b89c0c518b250e0c306de
Strider <strange.transistor@gmail.com>
2022-12-18 14:01:23 +0300

    reduce gulp log.

--- test/run.js
+++ test/run.js
@@ -3,5 +3,5 @@ var { spawnSync: run } = require('child_process')
 
 module.exports = (cmd, args, tmp) =>
 {
-	return run(cmd, args, { cwd: tmp(), stdio: 'inherit' })
+	return run(cmd, [ '-L', ...args ], { cwd: tmp(), stdio: 'inherit' })
 }
