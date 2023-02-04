c015702415a847ea8553e0ef5b9b349718fca5f1
Strider <strange.transistor@gmail.com>
2023-02-04 05:36:49 +0300

    handle less at sign in custom var.

--- fixt/frontend/gulpfile.js
+++ fixt/frontend/gulpfile.js
@@ -4,7 +4,8 @@ exports.default = require('metalpipe/prefab')('frontend', require('gulp'),
 	vars (opts, pkg)
 	{
 		var custom_opt = ((opts.hash || 'HASH') + '_' + pkg.version)
+		var custom_at = 'foo@barbarbar'
 
-		return { custom_opt }
+		return { custom_opt, custom_at }
 	},
 })
--- unit/less.js
+++ unit/less.js
@@ -4,9 +4,23 @@ var gulp_less = require('gulp-less')
 
 module.exports = function less ({ $from, exp_opts })
 {
+	var map = { ...exp_opts.as_map() }
+	for (var key in map)
+	{
+		var v = map[key]
+		if (typeof v === 'string')
+		{
+			if (v.indexOf('@') !== -1)
+			{
+				v = `~'${ v }'`
+				map[key] = v
+			}
+		}
+	}
+
 	return gulp_less(
 	{
 		paths: $from(),
-		globalVars: exp_opts.as_map(),
+		globalVars: map,
 	})
 }
