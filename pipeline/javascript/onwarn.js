481af473679f86488809255a4b2ac71e4219e5c7
Strider <strange.transistor@gmail.com>
2023-05-08 04:16:21 +0300

    warn ignore THIS_IS_UNDEFINED.

--- pipeline/javascript/onwarn.js
+++ pipeline/javascript/onwarn.js
@@ -2,6 +2,7 @@
 
 module.exports = function onwarn (warning)
 {
+	if (warning.code === 'THIS_IS_UNDEFINED') return
 	// https://github.com/rollup/rollup/blob/master/src/rollup/types.d.ts
 	// console.log(warning.chunk)
 	console.debug('Rollup (%s): %s', warning.code, warning.toString())
