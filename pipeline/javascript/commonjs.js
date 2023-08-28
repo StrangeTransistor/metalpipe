6909d53089c861bdcfcf6aed6f57936322ee2712
Strider <strange.transistor@gmail.com>
2023-08-28 22:14:46 +0300

    re-settle commonjs option (6ff92cb).

--- pipeline/javascript/commonjs.js
+++ pipeline/javascript/commonjs.js
@@ -9,6 +9,6 @@ module.exports = function commonjs ()
 	return commonjs(
 	{
 		requireReturnsDefault: 'preferred', /* auto */
-		defaultIsModuleExports: true, /* auto */
+		defaultIsModuleExports: 'auto', /* ← not covered (true/auto) */
 	})
 }
