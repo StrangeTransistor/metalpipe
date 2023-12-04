948f41e2816c7d5c430487add3d404e766e45b5d
Strider <strange.transistor@gmail.com>
2023-12-04 19:33:47 +0300

    warn ignore MODULE_LEVEL_DIRECTIVE.

--- pipeline/javascript/onwarn.js
+++ pipeline/javascript/onwarn.js
@@ -3,6 +3,7 @@
 module.exports = function onwarn (warning)
 {
 	if (warning.code === 'THIS_IS_UNDEFINED') return
+	if (warning.code === 'MODULE_LEVEL_DIRECTIVE') return
 	// https://github.com/rollup/rollup/blob/master/src/rollup/types.d.ts
 	// console.log(warning.chunk)
 	console.debug('Rollup (%s): %s', warning.code, warning.toString())
